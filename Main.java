cf499124df80fba48abc02a3918255f06ddda413
KARL LANCE SAGUILOT <2244364@slu.edu.ph>
2026-05-03 23:33:17 +0800

    Added Initial Code for Python Demo.

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public Main() {
         frame = new JFrame("Working Title");
-        frame.setSize(1000, 800);
+        frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         cardLayout = new CardLayout();
